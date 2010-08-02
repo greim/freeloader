020333a3fbc9d1bd16c2bed669ffd8657832b6a8
Greg Reimer <gregreimer@gmail.com>
2010-08-01 22:39:48 -0600

    modified demo

--- demo/my-widget.js
+++ demo/my-widget.js
@@ -1,9 +1,10 @@
-var words = "Every time you add one of these widgets to the page, freeloader runs an initialization function on it. The init function adds this text. . . . . . . . . . . . . San Dimas High School Football Rules!!!".split(/\s+/);
+var words = "Every time you add one of these widgets to the page, freeloader runs an initialization function on it. The init function displays the message \"loading...\" then adds this text after a 200ms delay.".split(/\s+/);
 FREELOADER.className('widget').onload(function(){
 	// initialize the widget
 	var widg = this;
+	widg.innerHTML = 'loading...';
 	window.setTimeout(function(){
 		widg.innerHTML = words.length ? words.shift() : '';
-		jQuery(widg).removeClass('loading');
+		jQuery(widg).addClass('loaded');
 	},200);
 });
